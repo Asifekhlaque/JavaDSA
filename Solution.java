998b47c2e81933de48cb90263e184c70edf808aa
user.name like "Asif Ekhlaque <asifekhlaque33@gmail.com>
2026-02-09 10:47:37 +0530

    Linked lIst Opration

--- Solution.java
+++ Solution.java
@@ -1,22 +1,17 @@
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
-
-
+import java.util.Scanner;
 
 public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(bufferedReader.readLine().trim());
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        double d = scan.nextFloat();
+        String s = scan.nextLine();
 
-        bufferedReader.close();
-        for(int i=1;i<=10;i++){
-            System.out.println(N+" x "+i+" = "+(N*i));
-        }
+        // Write your code here.
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
     }
 }
